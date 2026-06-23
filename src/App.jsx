85591dbd465f5ebfcbbde1b7586fa0853452f38f
gdlp <guadalupears1110@gmail.com>
2026-06-22 20:07:17 -0600

    a ver

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
-import PsicoLuz from './components/PsicoLuz'
+import { Routes, Route } from 'react-router-dom'; // Importa las rutas
+import PsicoLuz from './components/PsicoLuz';
+import Login from './components/Login'; // Asegúrate de tener este componente
 
 function App() {
-  return <PsicoLuz />
+  return (
+    <Routes>
+      <Route path="/" element={<PsicoLuz />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
